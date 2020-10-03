32d2885d18615929fd15699d86f727eeb848880f
Arijit Shaw <if.arijit@gmail.com>
2020-10-04 00:57:29 +0530

    add step 3 and 4

--- p02_ridge/ridge_regression.py
+++ p02_ridge/ridge_regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, RidgeCV, LassoCV
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
@@ -20,18 +20,42 @@ for lambd in lambdas:
     clf.fit(X, y)
     coefs.append(clf.coef_)
     errors.append(mean_squared_error(clf.coef_, w))
-    # print('Degree : %d Training Error %.3f Test Error %.3f'%(deg,train_rmse[-1],test_rmse[-1]))
 
-plt.figure(figsize=(20, 6))
-
-ax = plt.gca()
-ax.plot(lambdas, coefs)
-ax.set_xscale('log')
-plt.xlabel('alpha')
-plt.ylabel('weights')
-plt.title('Ridge coefficients as a function of the regularization')
-plt.axis('tight')
-
-plt.show()
+# plt.figure(figsize=(20, 6))
+#
+# ax = plt.gca()
+# ax.plot(lambdas, coefs)
+# ax.set_xscale('log')
+# plt.xlabel('alpha')
+# plt.ylabel('weights')
+# plt.title('Ridge coefficients as a function of the regularization')
+# plt.axis('tight')
+#
+# plt.show()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+
+lambdas = [0,4,1e10]
+rmse = []
+
+for lambd in lambdas:
+    clf = Ridge(alpha=lambd)
+    clf.fit(X, y)
+    clf.fit(X_train, y_train)
+    y_test_predicted = clf.predict(X_test)
+    rmse.append(np.sqrt(mean_squared_error(y_test,y_test_predicted)))
+    print('Lambda : %d | Test Error %.3f'%(lambd,rmse[-1]))
+
+
+lambdas = np.logspace(-6, 10, 200)
+clf = RidgeCV(alphas=lambdas).fit(X_train, y_train)
+y_test_predicted = clf.predict(X_test)
+rmse = np.sqrt(mean_squared_error(y_test,y_test_predicted))
+print('Best Lambda in Ridge | Test Error %.3f'%(rmse))
+
+
+lambdas = np.logspace(-6, 10, 200)
+clf = LassoCV(alphas=lambdas).fit(X_train, y_train)
+y_test_predicted = clf.predict(X_test)
+rmse = np.sqrt(mean_squared_error(y_test,y_test_predicted))
+print('Best Lambda in Lasso | Test Error %.3f'%(rmse))
