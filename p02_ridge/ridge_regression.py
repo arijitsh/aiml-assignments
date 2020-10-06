a6010f6c7a0345f962d6600f6b87774d5699d8ca
Arijit Shaw <if.arijit@gmail.com>
2020-10-06 15:15:25 +0530

    modifying errors

--- p02_ridge/ridge_regression.py
+++ p02_ridge/ridge_regression.py
@@ -8,12 +8,13 @@ from sklearn.metrics import mean_squared_error
 dataset = pd.read_csv('hitters.csv')
 
 coefs = []
+coefs0 = []
 errors = []
 w = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 X = dataset.iloc[:, 0:-2].values
 y = dataset.iloc[:, -2].values
 
-lambdas = np.logspace(-2, 10, 200)
+lambdas = np.logspace(-4, 8, 50)
 
 for lambd in lambdas:
     clf = Ridge(alpha=lambd)
@@ -21,17 +22,14 @@ for lambd in lambdas:
     coefs.append(clf.coef_)
     errors.append(mean_squared_error(clf.coef_, w))
 
-# plt.figure(figsize=(20, 6))
-#
-# ax = plt.gca()
-# ax.plot(lambdas, coefs)
-# ax.set_xscale('log')
-# plt.xlabel('alpha')
-# plt.ylabel('weights')
-# plt.title('Ridge coefficients as a function of the regularization')
-# plt.axis('tight')
-#
-# plt.show()
+ax = plt.gca()
+ax.plot(lambdas, coefs)
+ax.set_xscale('log')
+plt.xlabel('lambdas')
+plt.ylabel('coefficients')
+plt.title('Ridge coefficients as a function of the regularization')
+
+plt.show()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 
@@ -53,8 +51,6 @@ y_test_predicted = clf.predict(X_test)
 rmse = np.sqrt(mean_squared_error(y_test,y_test_predicted))
 print('Best Lambda in Ridge | Test Error %.3f'%(rmse))
 
-
-lambdas = np.logspace(-6, 10, 200)
 clf = LassoCV(alphas=lambdas).fit(X_train, y_train)
 y_test_predicted = clf.predict(X_test)
 rmse = np.sqrt(mean_squared_error(y_test,y_test_predicted))
